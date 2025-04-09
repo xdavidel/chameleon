90ff1fa8599bac9cf108b5cd363ad45d689eff66
David Delarosa <xdavidel@gmail.com>
2025-04-10 02:47:29 +0300

    Use new addModule api

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.createModule(.{
+    _ = b.addModule("chameleon", .{
         .root_source_file = b.path("src/chameleon.zig"),
         .target = target,
         .optimize = optimize,
